2c64fe59929f5432d4b7935f81d1e876a44b715f
benjamincode <benjaminsmith@ya.ru>
2014-01-05 09:57:49 +1000

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlitterApp::Application.config.secret_key_base = '439c4d76c5ba78644e4ae2b6486611452228351a0f24a6e4308314b2875a8380198d02b4b5465856035e7145d14b3c84cd7abe24439d5114b16315d225497d7a'
